cfac736d324b05c948b0b7317e15357b390f6a1c
pmaharjan124 <maharjanpuja@gmail.com>
2020-04-17 15:37:15 -0400

    first commit

---
+++ src/Hurricane.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+public class Hurricane {
+    public static void main(String[]args){
+        int speed;
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the Speed");
+        speed = sc.nextInt();
+
+        if(speed < 74) {
+            System.out.println("It's a normal air.")
+        }
+        else if ((speed >=74) && (speed <=95)){
+            System.out.println("Category 1");
+    else if((speed >=96) && (speed <=110)){
+                System.out.println("Category 1");
+    else if((speed >=111) && (speed <=129)){
+                    System.out.println("Category 1");
+     else if((speed >=130) && (speed <=156)){
+                        System.out.println("Category 1");
+       else(speed >157)
+                        { System.out.println("Category 5");
+
+                        }
+                    }
+
+                }
